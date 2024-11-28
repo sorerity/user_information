ee40745d799e61e95582b2fa6c3a0bc2a02be3a9
Sorerity <jdangelogsoon@gmail.com>
2024-11-28 21:28:01 +0800

    Added program title, also added the input for name which errors whenever you type numbers.

--- user_information.py
+++ user_information.py
@@ -1,25 +1,68 @@
-Create a program that ask user for personal information. 
-Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. 
-Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. 
-The program should ask user if want to input another person or exit.
+print("Personal Information:")
+while True:
+    name = input("Name: ")
+    if name.isalpha():
+        continue
+    else:
+        print("Error, please enter letters only.")
+        break
 
-print Personal Information
 
-establish a while true loop
-    ask user to input
-        Name
-        Age
-        Contact Num.
-        Address
-        Zip Code 
-    append each answer to the dict
-    add a dict
-ask the user if they still want to add another person
-if Y, loop back to the while true loop
-if N, BREAK
 
-open a .txt file 
-for each person in dict
-    write each info
 
-file handle close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Create a program that ask user for personal information. 
+#Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. 
+#Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. 
+#The program should ask user if want to input another person or exit.
+
+#print Personal Information
+
+#establish a while true loop
+    #ask user to input
+        #Name
+        #Age
+        #Contact Num.
+       # Address
+       # Zip Code 
+   # append each answer to the dict
+   # add a dict
+#ask the user if they still want to add another person
+#if Y, loop back to the while true loop
+#if N, BREAK
+
+#open a .txt file 
+#for each person in dict
+    #write each info
+
+#file handle close
