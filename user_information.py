bf178c86e7e0efde55243743ec9544b9e7d949b3
Sorerity <jdangelogsoon@gmail.com>
2024-11-28 21:17:26 +0800

    Pseudocode

---
+++ user_information.py
@@ -0,0 +1,25 @@
+Create a program that ask user for personal information. 
+Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. 
+Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. 
+The program should ask user if want to input another person or exit.
+
+print Personal Information
+
+establish a while true loop
+    ask user to input
+        Name
+        Age
+        Contact Num.
+        Address
+        Zip Code 
+    append each answer to the dict
+    add a dict
+ask the user if they still want to add another person
+if Y, loop back to the while true loop
+if N, BREAK
+
+open a .txt file 
+for each person in dict
+    write each info
+
+file handle close
