9cf841eba5ae0a47d0d5f3879a30520ae5f49373
Sorerity <jdangelogsoon@gmail.com>
2024-12-05 11:05:52 +0800

    Pseudocode.

--- information_finder.py
+++ information_finder.py
@@ -1,3 +1,15 @@
 Create another program that ask user for fullname.
 Find the full name in the txt file (output of program1). 
 Display the informations found in the txt file.
+
+Open the .txt file in read mode
+read all the lines from the file
+
+Ask user for the name they want to search for
+Read all lines from the File 
+Search for the name in the File
+    if found, display information
+    if not found, display "Name not found."
+
+Close File
+End
